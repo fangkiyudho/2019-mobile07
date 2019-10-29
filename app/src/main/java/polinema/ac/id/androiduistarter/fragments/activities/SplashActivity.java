2aebca315c4d9ef6ae99a2ad1f20fb9d20a2aa6d
fangkiyudho <fangkiyudho@gmail.com>
2019-10-29 17:43:47 +0700

    SplashScreen

---
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package polinema.ac.id.androiduistarter.fragments.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import polinema.ac.id.androiduistarter.R;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
